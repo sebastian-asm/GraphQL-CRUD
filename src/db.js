de1d75414c196b810c6719eaff71846cdbff0050
Sebastián Sánchez <sebastian.asm@outlook.com>
2021-12-10 19:02:01 -0300

    configuracion de mongo atlas url

--- src/db.js
+++ src/db.js
@@ -6,13 +6,13 @@ export default async function dbConnection() {
   // evitar multiples conexiones
   if (db) return db;
 
-  const url = 'mongodb://localhost:27017';
+  const url = process.env.MONGO_URI || 'mongodb://localhost:27017';
   const client = new MongoClient(url);
 
   try {
     await client.connect();
     console.log('Connected successfully to MongoDB');
-    db = client.db('graphql-test');
+    db = client.db('graphql-crud');
     return db;
   } catch (error) {
     console.log('Error connection to MongoDB', error);
