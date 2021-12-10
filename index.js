b7bc3bb592af9695a9d8cfe731058673bf652012
Sebastián Sánchez <sebastian.asm@outlook.com>
2021-12-10 18:24:03 -0300

    agregado archivos de las request creadas con Insomnia

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { graphqlHTTP } from 'express-graphql';
-import { readFileSync } from 'fs';
-import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { graphqlHTTP } from 'express-graphql';
+import { join, dirname } from 'path';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { readFileSync } from 'fs';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 
 import resolvers from './src/resolvers.js';
 
 const server = express();
-const port = process.env.PORT || 3001;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// configuraciones
+dotenv.config();
+server.use(cors());
 
 // definiendo el esquema
 const typeDefs = readFileSync(
@@ -18,6 +23,8 @@ const typeDefs = readFileSync(
 );
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-server.use('/api', graphqlHTTP({ schema }));
+server.use('/api', graphqlHTTP({ schema, graphiql: true }));
 
-server.listen(port, () => console.log('Server on port', port));
+server.listen(process.env.PORT, () =>
+  console.log('Server on port', process.env.PORT)
+);
